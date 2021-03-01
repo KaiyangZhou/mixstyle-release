f25e67e57722dd8d02c1a4734505297246988818
KaiyangZhou <k.zhou@surrey.ac.uk>
2021-03-01 20:51:16 +0800

    fix typo

--- imcls/run_batch2.sh
+++ imcls/run_batch2.sh
@@ -14,8 +14,8 @@ BATCH=128
 
 for SEED in $(seq ${START} ${END})
 do
-    bash run2.sh ${D2} ${D3} ${D4} ${D1} ${SEED} ${DATASET} ${ND} ${BATCH}
-    bash run2.sh ${D1} ${D3} ${D4} ${D2} ${SEED} ${DATASET} ${ND} ${BATCH}
-    bash run2.sh ${D1} ${D2} ${D4} ${D3} ${SEED} ${DATASET} ${ND} ${BATCH}
-    bash run2.sh ${D1} ${D2} ${D3} ${D4} ${SEED} ${DATASET} ${ND} ${BATCH}
+    bash run_single2.sh ${D2} ${D3} ${D4} ${D1} ${SEED} ${DATASET} ${ND} ${BATCH}
+    bash run_single2.sh ${D1} ${D3} ${D4} ${D2} ${SEED} ${DATASET} ${ND} ${BATCH}
+    bash run_single2.sh ${D1} ${D2} ${D4} ${D3} ${SEED} ${DATASET} ${ND} ${BATCH}
+    bash run_single2.sh ${D1} ${D2} ${D3} ${D4} ${SEED} ${DATASET} ${ND} ${BATCH}
 done
